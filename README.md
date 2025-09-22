2e0b80a939bf0a2bfc2034d98bf786663710763d
Alex <alexandra.marie0106@gmail.com>
2025-09-21 22:37:48 -0600

    Initial dev environment and first major release

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# vertical-teleport
-test commit
+# Vertical Teleport
+Configured block type (Default: LODESTONE) now acts as a instant "elevator" for vertical traversal.
+
+To set up an "elevator":
+1. Place two of the configured block types at preferred Y-levels within the same column
+2. Make sure the two blocks above each "elevator" block may safely contain a player (no LAVA, no solid blocks)
+
+To use an "elevator":
+1. Stand on an "elevator" block
+2. To go UP to another "elevator" block, right click the "elevator" block you're standing on
+3. To go DOWN to another "elevator" block, left click the "elevator" block you're standing on
---
+++ src/main/java/io/elyvara/vtp/VerticalTeleport.java
@@ -0,0 +1,116 @@
+package io.elyvara.vtp;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import net.kyori.adventure.text.Component;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.block.Action;
+import org.bukkit.Material;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+public class VerticalTeleport extends JavaPlugin implements Listener {
+	@Override
+	public void onEnable() {
+		saveDefaultConfig();
+		Bukkit.getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		final Material elevatorMaterial = Material.getMaterial(getConfig().getString("material-type"));
+		Block clickedBlock = event.getClickedBlock();
+		Player player = event.getPlayer();
+		Location playerLoc = player.getLocation();
+
+		// Check if left or right click
+		if (!event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && !event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+			return;
+		}
+
+		// Check if clicked block is null
+		if (clickedBlock == null) {
+			return;
+		}
+
+		// Check if clicked block is elevatorMaterial
+		if (!clickedBlock.getType().equals(elevatorMaterial)) {
+			return;
+		}
+
+		// Check if clicked block is below player
+		if (playerLoc.toCenterLocation().getX() == clickedBlock.getLocation().toCenterLocation().getX()
+				&& playerLoc.toCenterLocation().getZ() == clickedBlock.getLocation().toCenterLocation().getZ()
+				&& playerLoc.toCenterLocation().getY() > clickedBlock.getLocation().toCenterLocation().getY()) {
+			final int worldFloorY = -64;
+			final int worldCeilingY = 320;
+			World world = player.getWorld();
+			Location destination = player.getLocation().clone();
+			destination.setX(clickedBlock.getX() + 0.5);
+			destination.setZ(clickedBlock.getZ() + 0.5);
+
+			// Should safely cancel the event at this point
+			event.setCancelled(true);
+
+			if (event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+				// If left click --> check downward for elevator
+				for (double destY = clickedBlock.getY() - 2; destY > worldFloorY; destY--) {
+					destination.setY(destY);
+
+					if (isSafeElevator(destination, event)) {
+						if (!player.teleport(destination)) {
+							player.sendMessage(Component.text("Teleport failed for unknown reason."));
+						}
+						// Stop searching
+						break;
+					}
+				}
+			}
+			else if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+				// If right click --> check upward for elevator
+				for (double destY = clickedBlock.getY() + 4; destY < worldCeilingY; destY++) {
+					destination.setY(destY);
+					if (isSafeElevator(destination, event)) {
+						if (!player.teleport(destination)) {
+							player.sendMessage(Component.text("Teleport failed for unknown reason."));
+						}
+						// Stop searching
+						break;
+					}
+				}
+			}
+		}
+		else {
+			return;
+		}
+	}
+
+	public boolean isSafeElevator(Location loc, PlayerInteractEvent event) {
+		Block legDest = loc.getBlock();
+		Block headDest = legDest.getRelative(BlockFace.UP);
+		final Material elevatorMaterial = Material.getMaterial(getConfig().getString("material-type"));
+		Block potentialElevator = legDest.getRelative(BlockFace.DOWN);
+
+		if (!potentialElevator.getType().equals(elevatorMaterial)) {
+			// Not an elevator
+			return false;
+		}
+
+		if (legDest.isPassable() && headDest.isPassable() 
+				&& !legDest.getType().equals(Material.LAVA) 
+				&& !headDest.getType().equals(Material.LAVA)) {
+			// Safe elevator at loc
+			return true;
+		}
+		else {
+			// Not safe, notify player
+			event.getPlayer().sendMessage(Component.text("Destination elevator blocked or unsafe."));
+			return false;
+		}
+	}
+}
